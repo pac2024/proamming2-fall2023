64a933a4e6d5227056f320316ebb8c93654fe6f8
Paul Callahan <paulcallahan@Pauls-MacBook-Air.local>
2024-01-18 12:59:25 -0600

    push

--- final-project/home.js
+++ final-project/home.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Delay showing the popup for 5 seconds (5000 milliseconds)
     setTimeout(function() {
         document.getElementById('contact-popup').style.display = 'flex';
     }, 5000);
@@ -7,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function acceptContact() {
     alert('Great! Press OK and fill out our form!');
-    // Add the redirection logic here
     window.location.href = 'contact.html';
 }
 
